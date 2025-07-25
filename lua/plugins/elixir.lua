6d48acb6b41f0ea2687f2e5feb16663c235eab83
Gyeyoung Baek <gye976@gmail.com>
2025-07-25 18:28:28 +0900

    update elixir.lua

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -3,7 +3,8 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = { 
-	"nvim-tree/nvim-web-devicons" },
+	"nvim-tree/nvim-web-devicons" 
+  },
   },
   {
     "gye976/elixir.nvim",
@@ -17,72 +18,6 @@ return {
     },
 
     config = function()
-	    local ts_utils = require("nvim-treesitter.ts_utils")
-
-local function get_function_name()
-  local expr = ts_utils.get_node_at_cursor()
-  if not expr then return "" end
-
-  while expr do
-    if expr:type() == 'function_definition' then
-      break
-    end
-    expr = expr:parent()
-  end
-
-  if not expr then return "" end
-
-  -- expr:child(1) 대신 이름 노드 찾아보기
-  local name_node = expr:child(0)
-  if not name_node then return "[anonymous]" end
-
-  local ok, name = pcall(vim.treesitter.query.get_node_text, name_node, 0)
-  if ok and name ~= "" then
-    return name:gsub("\n", " ")  -- 줄바꿈 있으면 공백으로 바꿈
-  else
-    -- 노드 텍스트를 못 얻으면 노드 범위로 직접 텍스트 얻기
-    local start_row, start_col, end_row, end_col = expr:range()
-    local lines = vim.api.nvim_buf_get_lines(0, start_row, end_row + 1, false)
-
-    if #lines == 0 then
-      return "[anonymous]"
-    end
-
-	for i, line in ipairs(lines) do
-		print(string.format("line %d: %s", i, line))
-	end
-
-	local text = ""
-	for i, line in ipairs(lines) do
-		local brace_pos = string.find(line, "{")
-  line = line:gsub("^%s+", "")        -- 앞쪽 공백 제거
-  line = line:gsub("\t", " ")         -- 탭을 공백으로
-  line = line:gsub("%s+", " ")        -- 다중 공백을 하나로
-		if brace_pos then
-	    		text = text .. string.sub(line, 1, brace_pos - 1)
-	    		break
-	  	else
-	    		text = text .. line .. " "
-	  	end
-	end
-	
-	return text
-end
-end  -- 여기 꼭 닫기!
-
-
-local function update_winbar()
-  local text = get_function_name()
-  if text == "" then text = "" end
-  vim.opt.winbar = text
-end
-
-vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI", "BufEnter", "BufWinEnter"}, {
-  callback = update_winbar,
-})
-
-update_winbar()
-
 	local elixir = require("elixir")
 
 	vim.keymap.set('n', '<Leader><Left>', elixir.tab.prev, { noremap = true, silent = true })
@@ -92,13 +27,15 @@ update_winbar()
         vim.keymap.set("n", "<leader>ss", elixir.search.symbol, { desc = "Search Symbol" })
         vim.keymap.set("n", "<leader>sd", elixir.search.definition, { desc = "Search Definition" })
 
-        vim.keymap.set('n', 'gd', elixir.goto.definition, { desc = 'LSP: gd', noremap = true, silent = true })
-        vim.keymap.set('n', 'gr', elixir.goto.references, { desc = 'LSP: gr', noremap = true, silent = true })
+        vim.keymap.set('n', 'gf', elixir.goto.def_in_ctx, { desc = 'LSP: gf', noremap = true, silent = true })
+
+        vim.keymap.set('n', 'gd', elixir.goto.def, { desc = 'LSP: gd', noremap = true, silent = true })
+        vim.keymap.set('n', 'gr', elixir.goto.ref, { desc = 'LSP: gr', noremap = true, silent = true })
         vim.keymap.set('n', 'gi', elixir.goto.incoming, { desc = 'LSP: gi', noremap = true, silent = true })
         vim.keymap.set('n', 'go', elixir.goto.outgoing, { desc = 'LSP: go', noremap = true, silent = true })
 
-        vim.keymap.set('n', 'gD', elixir.goto.definition_in_newtab, { desc = 'LSP: gD', noremap = true, silent = true })
-        vim.keymap.set('n', 'gR', elixir.goto.references_in_newtab, { desc = 'LSP: gR', noremap = true, silent = true })
+        vim.keymap.set('n', 'gD', elixir.goto.def_in_newtab, { desc = 'LSP: gD', noremap = true, silent = true })
+        vim.keymap.set('n', 'gR', elixir.goto.ref_in_newtab, { desc = 'LSP: gR', noremap = true, silent = true })
         vim.keymap.set('n', 'gI', elixir.goto.incoming_in_newtab, { desc = 'LSP: gI', noremap = true, silent = true })
        vim.keymap.set('n', 'gO', elixir.goto.outgoing_in_newtab, { desc = 'LSP: gO', noremap = true, silent = true })
 
